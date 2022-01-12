f132a9359668741b388e14b021e4b93fe5691f46
tobias <tobias@TheNext.local>
2022-01-12 07:52:33 +0100

    adding experimental HP and BP, changes to playground examples

--- Digital2Pole.scala
+++ Digital2Pole.scala
@@ -16,3 +16,41 @@ class Digital2Pole(input: SiGen, cutoff: SiGen, reso: SiGen) extends CachedSiGen
   }
 
 }
+
+class Digital2PoleBP(input: SiGen, cutoff: SiGen, reso: SiGen) extends CachedSiGen {
+
+  var v0=0f
+  var v1=0f
+  val eps = 0.01f
+
+  def calculateNext(sid: Int):Float = {
+
+          val cut =  cutoff.getValue(sid)
+          val in = input.getValue(sid)
+          var fb = reso.getValue(sid)
+          fb =  fb + fb/(1.0f - cut + eps);
+          v0 = v0 + cut  * (in - v0 + fb * (v0 - v1))
+          v1 = v1 + cut  * (v0 - v1);
+          return (v0-v1)
+  }
+
+}
+
+class Digital2PoleHP(input: SiGen, cutoff: SiGen, reso: SiGen) extends CachedSiGen {
+
+  var v0=0f
+  var v1=0f
+  val eps = 0.01f
+
+  def calculateNext(sid: Int):Float = {
+
+          val cut =  cutoff.getValue(sid)
+          val in = input.getValue(sid)
+          var fb = reso.getValue(sid)
+          fb =  fb + fb/(1.0f - cut + eps);
+          v0 = v0 + cut  * (in - v0 + fb * (v0 - v1))
+          v1 = v1 + cut  * (v0 - v1);
+          return (in-v1)
+  }
+
+}
